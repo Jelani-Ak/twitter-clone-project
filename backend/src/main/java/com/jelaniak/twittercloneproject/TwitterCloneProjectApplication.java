378b82659501a5e252ce6771f03141f591040bfb
Jelani akoto <44904921+Jelani-Ak@users.noreply.github.com>
2021-11-22 22:36:05 +0000

    Small Changes
    - Added dependencies
    - Installed npm
    - Installed Angular Material

--- backend/src/main/java/com/jelaniak/twittercloneproject/TwitterCloneProjectApplication.java
+++ backend/src/main/java/com/jelaniak/twittercloneproject/TwitterCloneProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwitterCloneProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwitterCloneProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneProjectApplication.class, args);
+    }
 
 }
