9e7c8060a19d87645a4e7226a115737d0e1fed71
Jelani akoto <44904921+Jelani-Ak@users.noreply.github.com>
2021-11-22 20:32:55 +0000

    Initial commit

---
+++ src/main/java/com/jelaniak/twittercloneproject/TwitterCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jelaniak.twittercloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jelaniak/twittercloneproject/TwitterCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelaniak.twittercloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
