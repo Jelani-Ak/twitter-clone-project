1d380776c5b4f6979627a0b19e6de58180274f30
Jelani akoto <44904921+Jelani-Ak@users.noreply.github.com>
2022-10-03 19:34:27 +0100

    Correcting error in app.module.ts

--- frontend/twitter-clone-ui/src/app/app.module.ts
+++ frontend/twitter-clone-ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
